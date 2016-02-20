21dd6e3fdc04f74ba889c7c89c1b0dd3c3a98a05
Shama Hoque <shama.hoque@gmail.com>
2016-02-21 00:57:01 +0600

    add case sensitive filenames back

---
+++ app/components/todo/MainSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+import TodoItem from "./TodoItem.jsx";
+
+class MainSection extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    var Alltodos = this.props.Alltodos;
+    return (
+      <Grid>
+        <Row>
+          <Col md={12}>
+            {Alltodos.map((todo) => {
+              return (
+                <h1 key={todo.title}><TodoItem todo={todo}/>
+                </h1>
+              )
+            })}
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
+export default MainSection;
