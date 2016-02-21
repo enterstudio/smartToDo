6c29eff33dc7114805a7ebc5f3aed6f7cfa22ede
Shamiul Hoque <shoq9876@gmail.com>
2016-02-21 13:19:00 +0600

    feat: to-do sorted by category

--- app/components/todo/MainSection.jsx
+++ app/components/todo/MainSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import {Grid, Row, Col, Panel} from 'react-bootstrap';
 import TodoItem from "./TodoItem.jsx";
 
 class MainSection extends React.Component {
@@ -10,16 +10,55 @@ class MainSection extends React.Component {
 
   render() {
     var Alltodos = this.props.Alltodos;
+    var meetings = 0,
+    deadlines = 0,
+    registrations = 0,
+    reminders = 0;
     return (
       <Grid>
         <Row>
           <Col lg={8} md={8} sm={8} xs={8}>
+            <Panel header="Meetings" bsStyle="primary">
+            {Alltodos.map((todo, i) => {
+              meetings = (todo.category == "meeting")? meetings+1: meetings;
+              return (todo.category == "meeting")? (
+                <span key={i}>
+                  <h4> <TodoItem todo={todo}/> </h4>
+                </span>) : ((i+1 == Alltodos.length && meetings==0)&&(<span>No meeting emails found.</span>))
 
-            {Alltodos.map((todo) => {
-              return (
-                <h4 key={todo._id}> <TodoItem todo={todo}/> </h4>
-              )
             })}
+          </Panel>
+          <Panel header="Registrations / RSVP" bsStyle="success">
+            {Alltodos.map((todo, i) => {
+              registrations = (todo.category == "registration")? registrations+1: registrations;
+              return (todo.category == "registration")? (
+                <span key={i}>
+                  <h4> <TodoItem todo={todo}/> </h4>
+                </span>) : ((i+1 == Alltodos.length && registrations==0)&&(<span>No registration emails found.</span>))
+
+            })}
+            </Panel>
+            <Panel header="Reminders" bsStyle="info">
+            {Alltodos.map((todo, i) => {
+              reminders = (todo.category == "reminders")? reminders+1: reminders;
+              return (todo.category == "reminders")? (
+                <span key={i}>
+                  <h4> <TodoItem todo={todo}/> </h4>
+                </span>) : ((i+1 == Alltodos.length && reminders==0)&&(<span>No reminder emails found.</span>))
+
+            })}
+            </Panel>
+            <Panel header="Deadlines" bsStyle="warning">
+              {Alltodos.map((todo, i) => {
+                deadlines = (todo.category == "deadline")? deadlines+1: deadlines;
+                return (todo.category == "deadline")? (
+                  <span key={i}>
+                    <h4> <TodoItem todo={todo}/> </h4>
+                  </span>) : ((i+1 == Alltodos.length && deadlines == 0)&&(<span>No deadline emails found.</span>))
+
+              })}
+            </Panel>
+
           </Col>
         </Row>
       </Grid>
