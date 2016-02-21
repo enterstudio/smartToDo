5df6d29bda2a18ce4c6d4a8b6a2c5f8b64429e2b
Shamiul Hoque <shoq9876@gmail.com>
2016-02-21 16:13:32 +0600

    api fix

--- app/stores/TodoStore.jsx
+++ app/stores/TodoStore.jsx
@@ -93,6 +93,12 @@ function TodoStore() {
     var index = todos.indexOf(todo);
     todos[index].category = value;
     triggerListeners();
+    put(`/todos/api/todos/${todos[index]._id}`, todos[index]).then((data) => {
+      todos[index] = data;
+      triggerListeners();
+    }).catch((err) => {
+        console.log(err);
+    });
 
   }
 
